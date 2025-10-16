5aa05a70ee4f2a3e980e47aa11292ebe228a289e
Andrey Babanov <Andreuhelo@gmail.com>
2025-10-16 19:35:59 +0300

    PaintCalculator-Def | Добавлена функция счета

--- main.py
+++ main.py
@@ -20,3 +20,12 @@ class PaintCalculator:
             "синий": 1.15,
             "металлик": 1.3
         }
+
+        def calculate_price(self, detail: str, color: str) -> float:
+            # Получаем коэффициенты (если нет — берём 1.0)
+            detail_coeff = self.detail_coeffs.get(detail.lower(), 1.0)
+            color_coeff = self.color_coeffs.get(color.lower(), 1.0)
+
+            # Итоговая стоимость
+            price = self.base_price * detail_coeff * color_coeff
+            return price
